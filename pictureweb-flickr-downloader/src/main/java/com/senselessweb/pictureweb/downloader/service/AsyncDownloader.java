73e5748063180155c33c47010bbf326b040dd3c6
thomas <thomas@senselessweb.com>
2016-11-20 17:56:52 +0100

    Made downloader safe against unavailable picture sizes

--- pictureweb-flickr-downloader/src/main/java/com/senselessweb/pictureweb/downloader/service/AsyncDownloader.java
+++ pictureweb-flickr-downloader/src/main/java/com/senselessweb/pictureweb/downloader/service/AsyncDownloader.java
@@ -3,16 +3,22 @@ package com.senselessweb.pictureweb.downloader.service;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.util.Collection;
+import java.util.List;
 import java.util.concurrent.Future;
+import java.util.stream.Collectors;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Service;
 
 import com.flickr4java.flickr.Flickr;
+import com.flickr4java.flickr.FlickrException;
 import com.flickr4java.flickr.photos.Photo;
 import com.flickr4java.flickr.photos.Size;
+import com.google.common.collect.Lists;
 import com.google.common.util.concurrent.Futures;
 import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
@@ -33,6 +39,16 @@ import com.senselessweb.pictureweb.flickr.AuthenticatedFlickrProvider;
 public class AsyncDownloader {
 
   private static final Log log = LogFactory.getLog(AsyncDownloader.class);
+  private static final List<Integer> SIZES = Lists.newArrayList(
+      Size.ORIGINAL,  
+      Size.LARGE_1600,  
+      Size.LARGE,  
+      Size.MEDIUM_800,  
+      Size.MEDIUM_640,  
+      Size.MEDIUM,  
+      Size.SMALL_320,  
+      Size.SMALL);
+  
   private final AuthenticatedFlickrProvider flickr;
   private final StoredPhotos storageService;
 
@@ -70,7 +86,7 @@ public class AsyncDownloader {
         log.info("Starting download of photo " + photo.getId() + " in size " + sizeString + " to: " + target);
         final Flickr f = flickr.getIfAuthenticated().orElseThrow(() -> new IllegalStateException("Not authenticated"));
         try {
-          try (final InputStream is = f.getPhotosInterface().getImageAsStream(photo, size)) {
+          try (final InputStream is = f.getPhotosInterface().getImageAsStream(photo, getValidSize(f, photo, size))) {
             try (final FileOutputStream fos = new FileOutputStream(target)) {
               IOUtils.copy(is, fos);
               log.info("Finished download of photo " + photo.getId() + " in size " + sizeString + " to: " + target);
@@ -81,7 +97,21 @@ public class AsyncDownloader {
           throw new RuntimeException(e);
         }
       }
+
     };
+  }
+  
+  private int getValidSize(final Flickr f, final Photo photo, final int requestedSize) throws FlickrException {
+    final Collection<Integer> sizes = f.getPhotosInterface().getSizes(photo.getId()).stream().map(s -> s.getLabel()).collect(Collectors.toSet());
+    if (CollectionUtils.isEmpty(sizes)) {
+      throw new IllegalStateException("Picture " + photo + " not available in any sizes");
+    }
+    for (int i = SIZES.indexOf(requestedSize); i < SIZES.size(); i++) {
+      if (sizes.contains(SIZES.get(i))) {
+        return SIZES.get(i);
+      }
+    }
+    return sizes.iterator().next();
   }
   
   @HystrixCommand
--- pictureweb-flickr-fetcher/src/main/java/com/senselessweb/pictureweb/fetcher/service/AlbumCustomDataFetcher.java
+++
@@ -1,5 +0,0 @@
-package com.senselessweb.pictureweb.fetcher.service;
-
-public class AlbumCustomDataFetcher {
-
-}
