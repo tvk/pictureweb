38f54167becf7eff208fe89e400243028327e9f4
thomas <thomas@senselessweb.com>
2016-12-04 10:54:57 +0100

    Hide some pictures

--- pictureweb-web/src/main/java/com/senselessweb/pictureweb/web/service/ClientAlbumService.java
+++ pictureweb-web/src/main/java/com/senselessweb/pictureweb/web/service/ClientAlbumService.java
@@ -1,11 +1,13 @@
 package com.senselessweb.pictureweb.web.service;
 
 import java.util.Collection;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
 import com.google.common.base.Preconditions;
+import com.google.common.collect.Sets;
 import com.senselessweb.pictureweb.datastore.client.AlbumService;
 import com.senselessweb.pictureweb.web.domain.ClientAlbum;
 
@@ -13,6 +15,10 @@ import com.senselessweb.pictureweb.web.domain.ClientAlbum;
 public class ClientAlbumService {
 
   public static final String beanName = "albums";
+
+  private static final Set<String> hiddenAlbums = Sets.newHashSet(
+      "72157647261406841", "72157645129642036", "72157644769041219", "72157652634775562");
+
 
   private final AlbumService albumService;
   private final AlbumConverter albumConverter;
@@ -24,11 +30,15 @@ public class ClientAlbumService {
   }
 
   public Collection<ClientAlbum> getAlbums() {
-    return albumService.getAll().stream().map(albumConverter).collect(Collectors.toList());
+    return albumService.getAll().stream()
+        .filter(a -> !hiddenAlbums.contains(a.getId()))
+        .map(albumConverter).collect(Collectors.toList());
   }
 
   public Collection<ClientAlbum> getAlbumsWithGeo() {
-    return albumService.getAll().stream().map(albumConverter)
+    return albumService.getAll().stream()
+        .filter(a -> !hiddenAlbums.contains(a.getId()))
+        .map(albumConverter)
         .filter(a -> a.getGeo() != null).collect(Collectors.toList());
   }
 
